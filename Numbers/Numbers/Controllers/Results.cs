0ff46be02a333b5365534dbb282f512bc5c347ad
Kun <sdytjkgala@hotmail.com>
2019-12-17 11:47:10 -0500

    BoardView change

--- Numbers/Numbers/Controllers/Results.cs
+++ Numbers/Numbers/Controllers/Results.cs
@@ -6,6 +6,8 @@ public class Results
     public bool ValidMove { get; set; }
     public string Notification { get; set; }
     public Errors ErrorType { get; set; }
+    public Operations OperationType { get; set; }
     public enum Errors { InvalidMoveLength, InvalidHand, InvalidNumber, InvalidCalculation }
+    public enum Operations {Plus, Minus, Mutiple, Division }
 
 }
