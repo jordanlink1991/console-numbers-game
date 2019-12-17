96f75a442e07c852c85625b91c5a5bc3ea606703
Jordan Link <jordan.link@moodys.com>
2019-12-17 11:41:07 -0500

    Adding additional contructor to Player and Hand

--- Numbers/Numbers/Model/Hand.cs
+++ Numbers/Numbers/Model/Hand.cs
@@ -2,12 +2,25 @@
 
 public class Hand
 {
-	public int Value;
-    public string Tag;
+	/// <summary>
+	/// Tag (or name) of the hand
+	/// </summary>
+	public string Tag { get; set; }
+	
+	/// <summary>
+	/// Value of the hand
+	/// </summary>
+	public int Value { get; set; }
 
 	public Hand()
 	{
         Tag = "";
 		Value = 0;
+	}
+
+	public Hand(string tag, int value)
+	{
+		Tag = tag ?? string.Empty;
+		Value =  value;
 	}
 }
--- Numbers/Numbers/Model/Player.cs
+++ Numbers/Numbers/Model/Player.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 
 public class Player
 {
-	public List<Hand> Hands { get; set; }
+	/// <summary>
+	/// Name of the player
+	/// </summary>
     public string Name { get; set; }
+
+	/// <summary>
+	/// Set of Hands of the player
+	/// </summary>
+	public List<Hand> Hands { get; set; }
 
 	public Player()
 	{
 		Hands = new List<Hand>();
         Name = "Player - Master";
+	}
+
+	public Player(string name, List<Hand> hands)
+	{
+		Name = name ?? string.Empty;
+		Hands = hands ?? new List<Hand>();
 	}
 }
