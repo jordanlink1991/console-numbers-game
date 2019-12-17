c020327e4e3e96e01df6068d0a966a79d60e2a02
Kenny <k.hungria94@gmail.com>
2019-12-17 12:06:50 -0500

    Validation checks

--- Numbers/Numbers/Controllers/Results.cs
+++ Numbers/Numbers/Controllers/Results.cs
@@ -7,7 +7,13 @@ public class Results
     public string Notification { get; set; }
     public Errors ErrorType { get; set; }
     public Operations OperationType { get; set; }
-    public enum Errors { InvalidMoveLength, InvalidHand, InvalidNumber, InvalidCalculation }
-    public enum Operations {Plus, Minus, Mutiple, Division }
+    public enum Errors { InvalidMoveLength, InvalidHand, InvalidNumber, InvalidCalculation, NotSet }
+    public enum Operations {Plus, Minus, Mutiple, Division, NotSet }
+
+    public Results()
+    {
+        ErrorType = Errors.NotSet;
+        OperationType = Operations.NotSet;
+    }
 
 }
