95c47766e050753d5785a7b41a359f0ffd2d98f6
Jordan Link <jordan.link@moodys.com>
2019-12-17 13:23:49 -0500

    Board use of interpreter and view

--- Numbers/Numbers/Board.cs
+++ Numbers/Numbers/Board.cs
@@ -17,19 +17,36 @@ namespace Numbers
 			// if(result.IsInvalid)
 			//		return;
 
-			// BoardView.FormatWelcome();			
+			//BoardView.FormatWelcome();			
 
 			// Initialize players
-			Player currentPlayer;
-			List<Player> otherPlayers;
-			InitializePlayers(2, 0, 2, out currentPlayer, out otherPlayers);			
+			List<Player> players = InitializePlayers(2, 0, 2);
+
+			// Pop the first player
+			Player currentPlayer = players[0];
+			List<Player> otherPlayers = new List<Player>(players);
+			otherPlayers.RemoveAt(0);
 
 			// Run forever!
 			while (true)
 			{
-				// BoardView.FormatInput(currentPlayer);
-				Console.Write("Input (e for exit): ");
+				// Print state of Board
+				Console.WriteLine(BoardView.FormatPlayer(players));
+
+				// Read input
+				Console.Write(BoardView.FormatInputRequest(currentPlayer));
 				string input = Console.ReadLine();
+
+				Results result = Interpreter.ValidateMove(currentPlayer, otherPlayers, input);
+				if (result.Help)
+				{
+					Console.WriteLine(BoardView.FormatHelp());
+				}
+				else if(!result.ValidMove)
+				{
+					Console.WriteLine(BoardView.FormatError(result.ErrorType));
+				}
+
 				// var result = Interpretor.Interpret(currentPlayer, otherPlayers, input);
 				// if(result.IsHelp)
 				//     BoardView.FormatHelp();
@@ -65,27 +82,26 @@ namespace Numbers
 		/// <param name="handsPerPlayer"></param>
 		/// <param name="currentPlayer"></param>
 		/// <param name="otherPlayers"></param>
-		public static void InitializePlayers(int humanPlayers, int computerPlayers, int handsPerPlayer, out Player currentPlayer, out List<Player> otherPlayers)
+		public static List<Player> InitializePlayers(int humanPlayers, int computerPlayers, int handsPerPlayer)
 		{
-			otherPlayers = new List<Player>();
+			List<Player> players = new List<Player>();
 
 			// Initialize the hands
-			string tag = handsPerPlayer <= 2 ? HAND_TAGS_TWO : HAND_TAGS_MORE;
+			string tag = (handsPerPlayer <= 2 ? HAND_TAGS_TWO : HAND_TAGS_MORE);
+			
 			List<Hand> hands = new List<Hand>();
 			for (int i = 0; i < handsPerPlayer; i++)
 				hands.Insert(0, new Hand(tag[i].ToString(), 1));
 
 			// Add humans
 			for (int i = 0; i < humanPlayers; i++)
-				otherPlayers.Add(new Player(i.ToString(), new List<Hand>(hands)));
+				players.Add(new Player($"Player {(i+1).ToString()}", new List<Hand>(hands)));
 
 			// Add computer
-			for (int i = 0; i < humanPlayers; i++)
-				otherPlayers.Add(new Player(i.ToString(), new List<Hand>(hands), false));
+			for (int i = 0; i < computerPlayers; i++)
+				players.Add(new Player($"Computer {(i + 1).ToString()}", new List<Hand>(hands), false));
 
-			// Pop the first player
-			currentPlayer = otherPlayers[0];
-			otherPlayers.RemoveAt(0);
+			return players;
 		}
 	}
 }
