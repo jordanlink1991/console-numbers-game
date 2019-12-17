598a88c87bec8d2228c0ccdc9ca3a6e839ec96ef
Kun <sdytjkgala@hotmail.com>
2019-12-17 12:58:13 -0500

    AI class initial commit

---
+++ Numbers/Numbers/Controllers/AI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Numbers.Controllers
+{
+    public class AI
+    {
+        public static void BruteForce()
+        {
+
+        }
+
+        public static void Aggressive()
+        {
+
+        }
+
+        public static void Defensive()
+        {
+
+        }
+    }
+}
